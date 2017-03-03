8f40a2f8c8be4ef52c65ecae3427b588aec2a79e
Senthil <senthilr2007@gmail.com>
2017-03-03 15:57:31 +0530

    build

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 8080);
+app.use(express.static("www")); // Our Ionic app build is in the www folder (kept up-to-date by the Ionic CLI using 'ionic serve')
+
+
+
+// Initialize the app.
+app.listen(80), function () {
+    console.log("You're a wizard, Harry. I'm a what? Yes, a wizard, on port", app.get('port'));
+});
+
+// Error handler for the api
+function handleError(res, reason, message, code) {
+  console.log("API Error: " + reason);
+  res.status(code || 500).json({"Error": message});
+}
